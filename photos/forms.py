34fb489341a55f4d211b1437bb20b1b21810c4b3
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-09-20 15:25:01 -0500

    se agrega metodo clean de django que ayuda para las validaciones, en el modelo de PhotoForm se crea y realizamos nuestras validaciones personalizadas

--- photos/forms.py
+++ photos/forms.py
@@ -1,6 +1,15 @@
+from django.core.exceptions import ValidationError
+
 from photos.models import Photo
 from django.forms import ModelForm
 
+BADWORDS = (
+            "Caranabo",
+            "Carapan",
+            "Chimpamonas",
+            "Chupaescrotos",
+            "Cuerpoescombro"
+            )
 
 class PhotoForm(ModelForm):
 
@@ -8,3 +17,16 @@ class PhotoForm(ModelForm):
         model = Photo
         # fields = ['visibility', 'license'] seleccionamos el orden de los campos a mostrar.
         exclude = ['owner']
+
+    def clean(self):
+        """
+        Valida que la descripcion no contenga ninguna palabrota
+        :return: diccionario con los datos limpios y validados
+        """
+        cleaned_data = super().clean() # obtenelos los datos del formulario limpios que hace django por nosotros.
+        descripcion = cleaned_data.get('description', '')
+        for badword in BADWORDS:
+            if badword in descripcion:
+                raise ValidationError("La palabra {0} no esta permitida".format(badword))
+        return cleaned_data
+
