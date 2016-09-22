0df2e7621aa468f8188da00092a2d24a8a03523e
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-09-22 15:45:29 -0500

    se crea api POST para crear un usuario

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 __author__ = 'kas'
@@ -8,4 +8,19 @@ class UserSerializer(serializers.Serializer):
 
     first_name = serializers.CharField()  # la variable debe tener mismo nombre del campo
     last_name = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+    email = serializers.CharField()
+
+    def create(self, validated_data):
+        instance = User()
+        instance.first_name = validated_data.get('first_name')
+        instance.last_name = validated_data.get('last_name')
+        instance.username = validated_data.get('username')
+        instance.password = validated_data.get('password')
+        instance.email = validated_data.get('email')
+        instance.save()
+        return instance
+
+
 
