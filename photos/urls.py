fe30fee3c5fe86571007ac7f960b5522eef0ed29
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-09-23 20:59:32 -0500

    API con ModelSerializer de rest_framework, endpoint de listado y creacion de fotos

--- photos/urls.py
+++ photos/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+
+from photos.api import PhotoListAPI
 from photos.views import HomeView, PhotoDetailView, PhotoCreationView, PhotoListView
 
 
@@ -9,5 +11,9 @@ urlpatterns = [
     url(r'^photos/(?P<pk>[0-9]+)$', PhotoDetailView.as_view(), name='photos_detail'),
     url(r'^create$', PhotoCreationView.as_view(), name='photos_create'),
     url(r'^photos/$', PhotoListView.as_view(), name='photos_my_photos'),
-    url(r'^$', HomeView.as_view(), name='photos_home')
+    url(r'^$', HomeView.as_view(), name='photos_home'),
+
+
+    # URLS APis
+    url(r'^api/1.0/photos/$', PhotoListAPI.as_view(), name='api_photos_list'),
 ]
