045f5829e1c55112d103dc782b6fae0e945b2cf4
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-09-23 21:17:16 -0500

    endpoint con ModelSerializer, para detalle, actualizacion y borrado de fotos

--- photos/api.py
+++ photos/api.py
@@ -1,10 +1,20 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
 
 
 class PhotoListAPI(ListCreateAPIView):
+    """
+    End point de listado y creacion de fotos
+    """
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
 
+
+class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
+    """
+    Endpoint de detalle, actualizacion y borrado de fotos
+    """
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
