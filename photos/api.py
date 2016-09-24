4f124a6505dc699d149a03fc1f4f0378c3553082
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-09-23 21:45:01 -0500

    Terminamos nuestra API de fotos

--- photos/api.py
+++ photos/api.py
@@ -1,7 +1,7 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 from photos.models import Photo
-from photos.serializers import PhotoSerializer
+from photos.serializers import PhotoSerializer, PhotoListSerializer
 
 
 class PhotoListAPI(ListCreateAPIView):
@@ -9,7 +9,11 @@ class PhotoListAPI(ListCreateAPIView):
     End point de listado y creacion de fotos
     """
     queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
+    # serializer_class = PhotoListSerializer
+
+    # se comenta la linea anterior para permitir al metodo siguiente seleccionar que serializador escoger.
+    def get_serializer_class(self):
+        return PhotoSerializer if self.request.method == 'POST' else PhotoListSerializer
 
 
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
--- photos/serializers.py
+++ photos/serializers.py
@@ -7,3 +7,9 @@ class PhotoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Photo
+
+
+class PhotoListSerializer(PhotoSerializer):
+
+    class Meta(PhotoSerializer.Meta):  # heredar la clase meta de la otra clase PhotoSerializer
+        fields = ("id", "name", "url")
