17f11ed6088a4900f02569327e2599bf1b5e6157
Nataly Contreras <nataly.contreras@cidesi.edu.mx>
2016-09-25 19:10:56 -0500

    se implementa filtrado y ordenado a la API

--- photos/api.py
+++ photos/api.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
@@ -10,6 +11,9 @@ from photos.views import PhotoQueryset
 class PhotoViewSet(ModelViewSet):
 
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    search_fields = ('name', 'description', )
+    order_fields = ('owner', 'created_at', 'name', 'id', )
+    filter_backends = (filters.SearchFilter, filters.OrderingFilter, )
 
     def get_queryset(self):
         return PhotoQueryset.get_photos_by_user(self.request.user)
